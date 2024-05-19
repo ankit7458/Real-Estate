c488b41e15af6bb41b89a93703af61f88509dae5
ankit7458 <ankit.cni99@gmail.com>
2024-05-19 15:41:26 +0530

    new data

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import {BrowserRouter ,Routes ,Route} from "react-router-dom"
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Signin from "./Pages/Signin";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+
 
 function App() {
   return(
-    <h1>Sucess!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
