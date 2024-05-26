508aab2e2429a0a4a1a2467cc3d9924ed03a3527
ankit7458 <ankit.cni99@gmail.com>
2024-05-26 20:10:50 +0530

    working on signup functionality

--- Api/controller/auth.controller.js
+++ Api/controller/auth.controller.js
@@ -3,8 +3,8 @@ import bcryptjs from "bcryptjs"
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    const hashpassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashpassword });
+    const hashpassword = await bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashpassword});
     try {
         await newUser.save();
         res.status(201).json("user created Succefull!")
--- Client/src/Pages/SignUp.jsx
+++ Client/src/Pages/SignUp.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log(formData);
+  const handleSubmut = async (e) => {
+    e.preventDefault();
+    const res = await fetch ('/api/auth/signup',{
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
-      <form className='flex flex-col gap-4'>
-        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' />
-        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
+      <form onSubmit={handleSubmut} className='flex flex-col gap-4'>
+        <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+        <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'onChange={handleChange} />
         <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' />
         <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
       </form>
