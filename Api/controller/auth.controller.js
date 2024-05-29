a22e71eca2c1d997a5fa2f248767f8c7d382159d
ankit7458 <ankit.cni99@gmail.com>
2024-05-30 00:28:26 +0530

    u code

--- Api/controller/auth.controller.js
+++ Api/controller/auth.controller.js
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    const hashpassword = await bcryptjs.hashSync(password, 10);
+    const hashpassword = bcryptjs.hashSync(password, 10);
     const newUser = new User({ username, email, password: hashpassword });
     try {
         await newUser.save();
